27354c0f2d9d9b8be7dc8fe8df36f62de3398d26
Aurélien Cros <aure.cros@gmail.com>
2023-10-24 12:01:20 +0200

    Corrected meta data

--- kittymachine.php
+++ kittymachine.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Kitty Machine
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       The Kitty Machine will grant you much happiness by providing endless furry friends !
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Aurélien Cros
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       kittymachine
--- readme.txt
+++ readme.txt
@@ -1,12 +1,12 @@
 === Kitty Machine ===
-Contributors:      The WordPress Contributors
+Contributors:      Aurélien Cros
 Tags:              block
 Tested up to:      6.1
 Stable tag:        0.1.0
 License:           GPL-2.0-or-later
 License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 
-Example block scaffolded with Create Block tool.
+The Kitty Machine will grant you much happiness by providing endless furry friends !
 
 == Description ==
 
